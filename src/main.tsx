eadee706eb1a935ad5e81ac3738e6b4d189a6f84
Ekaterina Miakisheva <magistr25@inbox.ru>
2025-01-09 12:18:12 +0600

    chore(deps): install sass and configure SCSS support

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import './styles/global.scss';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
