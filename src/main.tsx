2b1839931249fe0a20a75acd47c182439aa8f126
Ekaterina Miakisheva <magistr25@inbox.ru>
2025-01-11 20:46:07 +0600

    feat(router): add React Router setup with Apollo Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
 import './styles/main.scss';
 
+import {ApolloProvider} from "@apollo/client";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import {ApolloProvider} from "@apollo/client";
-import client from "./apollo/client.ts";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+import client from "./apollo/client.ts";
+import App from "./App.tsx";
+
+// eslint-disable-next-line no-undef
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <ApolloProvider client={client}>
-    <App />
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<App />}>
+                      {/*<Route index element={<HomePage />} />*/}
+                      {/*<Route path="/login" element={<LoginPage />} />*/}
+                  </Route>
+              </Routes>
+          </BrowserRouter>
       </ApolloProvider>
   </StrictMode>,
 )
