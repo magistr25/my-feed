aef261af6a89fad99fac81d9aa781a6ba2b9014b
Ekaterina Miakisheva <magistr25@inbox.ru>
2025-01-11 20:43:40 +0600

    fix(apollo): handle localStorage access error in Apollo Client setup

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,11 +1,19 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+let token = '';
+try {
+    token = localStorage.getItem('token') || '';
+} catch (error) {
+    console.warn('localStorage недоступен:', error);
+}
+
 const client = new ApolloClient({
     uri: 'https://internship-social-media.purrweb.com/graphql',
     cache: new InMemoryCache(),
     headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}` || '',
+        Authorization: token ? `Bearer ${token}` : '',
     },
 });
 
 export default client;
+
