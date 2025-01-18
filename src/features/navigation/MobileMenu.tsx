559c6c05be825510c51c88f2cbaee3d9470b129d
Ekaterina Miakisheva <magistr25@inbox.ru>
2025-01-18 10:26:13 +0600

    refactor(ui): update styles for MobileMenu

--- src/features/navigation/MobileMenu.tsx
+++ src/features/navigation/MobileMenu.tsx
@@ -22,20 +22,22 @@ const MobileMenu: FC<MobileMenuProps> = ({isOpen, onClose}) => {
             className={`mobile-menu ${isOpen ? 'mobile-menu_open' : ''}`}
         >
             <div className="mobile-menu__content" onClick={(e) => e.stopPropagation()}>
-                <div className="mobile-menu__content__header">
-                    <div className="mobile-menu__content__header_up">
-                        <svg width="219" height="28" viewBox="0 0 219 28" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M0 -2H219C216.894 -1.39731 215.418 0.495246 215.345 2.68446L215.317 3.5618C215.317 17.0587 204.375 28 190.878 28H28.1217C14.6249 28 3.68349 17.0587 3.68349 3.5618L3.65455 2.68447C3.58233 0.49525 2.10586 -1.39731 0 -2Z"
-                                fill="black"/>
-                        </svg>
-                    </div>
-                    <div className="mobile-menu__content__header_down">
-                        <button className="mobile-menu__close" onClick={onClose}>
-                            <img src={closeIcon} alt="Close menu"/>
-                        </button>
-                        <Logo/>
+                <div className="mobile-menu__content__header-container">
+                    <div className="mobile-menu__content__header">
+                        <div className="mobile-menu__content__header_up">
+                            <svg width="219" height="28" viewBox="0 0 219 28" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M0 -2H219C216.894 -1.39731 215.418 0.495246 215.345 2.68446L215.317 3.5618C215.317 17.0587 204.375 28 190.878 28H28.1217C14.6249 28 3.68349 17.0587 3.68349 3.5618L3.65455 2.68447C3.58233 0.49525 2.10586 -1.39731 0 -2Z"
+                                    fill="black"/>
+                            </svg>
+                        </div>
+                        <div className="mobile-menu__content__header_down">
+                            <button className="mobile-menu__close" onClick={onClose}>
+                                <img src={closeIcon} alt="Close menu"/>
+                            </button>
+                            <Logo/>
+                        </div>
                     </div>
                 </div>
                 <div className="mobile-menu__profile">
@@ -79,4 +81,4 @@ const MobileMenu: FC<MobileMenuProps> = ({isOpen, onClose}) => {
     );
 };
 
-export default MobileMenu;
+            export default MobileMenu;
