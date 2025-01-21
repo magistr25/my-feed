e7efdb1467132f53bf112e4298ea5f9d02ddbb82
Ekaterina Miakisheva <magistr25@inbox.ru>
2025-01-21 19:22:11 +0600

    style(auth): configure dark theme styles for login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {useEffect} from "react";
 import { Outlet, useLocation } from 'react-router-dom';
 
 import Header from '@/widgets/Header/Header';
@@ -5,6 +6,17 @@ import Header from '@/widgets/Header/Header';
 const App = () => {
     const location = useLocation();
     const hideHeaderPaths = ['/login', '/register']; // Пути, где не нужно показывать Header
+
+    useEffect(() => {
+        // Убираем старые классы
+        window.document.body.className = '';
+        // Добавляем класс на основе маршрута
+        if (location.pathname === '/login') {
+            window.document.body.classList.add('body-login');
+        } else if (location.pathname === '/register') {
+            window.document.body.classList.add('body-register');
+        }
+    }, [location]);
 
     return (
         <div className="app">
